b94ad621120b9416cdaab38c7d578cbc7dc8ac46
emazack <em.zaccaria@gmail.com>
2024-12-10 12:09:31 +0100

    print elements and create a dinamic html for elements and buttons

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log("Js attivo")
+import { menuArray } from "/data.js"
+
+const menuList = document.getElementById("menu-list")
+
+menuArray.map((element) => {
+    const { name, ingredients, id, price, emoji } = element
+    const htmlElement = document.createElement("li")
+    htmlElement.className = 'element';
+    htmlElement.innerHTML = 
+            `            
+            <div class="container">
+                <span class="emoji">${emoji}</span>
+            </div>
+            <div class="info">
+                <div class="name">
+                    ${name}
+                </div>
+                <div class="ingredients">
+                    ${ingredients}
+                </div>
+                <div class="price">
+                    $${price}
+                </div>
+            </div>
+            <div class="button">
+                <button id=${id} class="add-to">
+                    +
+                </button>
+            </div>
+            `;
+    menuList.appendChild(htmlElement)
+})
+
+const getPriceFromClickedButton = (buttonId) => {
+    const selectedEl = menuArray.find(product => product.id === buttonId)
+    return selectedEl.price
+}
+
+const addToButtonList = document.querySelectorAll(".add-to")
+addToButtonList.forEach((button) => {
+    button.addEventListener("click", getPriceFromClickedButton(button.id))
+});
+
+
